559942641c42fd0d9fcba09fcc78aefe4f675d0f
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-19 14:20:57 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        char b = 'A';
+        char a = 'B';
+
+        int valoreAsciiA = (int) a;
+        int valoreAsciiB = (int) b;
+
+        int somma = valoreAsciiA + valoreAsciiB;
+        System.out.println(somma);
+
+
+    }
+}
